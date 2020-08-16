4ca1f4a8d38ec173d8717fa081ed80f9d017be30
Richardson Lima <contatorichardsonlima@gmail.com>
2020-08-16 19:20:10 -0300

    adding some new methods to create microservices

--- lib/ecs-fargate-stack.ts
+++ lib/ecs-fargate-stack.ts
@@ -2,35 +2,77 @@ import * as cdk from '@aws-cdk/core';
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
-import { Cluster } from '@aws-cdk/aws-ecs';
+import { Cluster, FargateTaskDefinition, TaskDefinition } from '@aws-cdk/aws-ecs';
+import { clearScreenDown } from 'readline';
 
 
 export class EcsFargateStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
-    const vpc = ec2.Vpc.fromLookup(this, "vpc", {
-        vpcId: "vpc-0a0a0a0a0a00a0a00a0a0a0a00a0a0a",
-        //tags:{"Name:" ""},
-        //vpcName: "",
-    });
+    readonly vpc : ec2.IVpc; //interface IVpc
+    readonly cluster : ecs.ICluster // Class Cluster
+    readonly loadBalancerFargateService : ecs_patterns.ApplicationLoadBalancedFargateService;
+    readonly securityGroups: ec2.ISecurityGroup[];
 
-    const cluster = new ecs.Cluster(this, "EcsClusterStack01", {
-        vpc: vpc 
-    
-    });
-    const taskDefinition = new ecs.FargateTaskDefinition(this, "FargateTaskDef");
-    taskDefinition.addContainer('AppContainer',{
-        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-        memoryLimitMiB: 512,
-    });
-    // Initiating a ECS Fargate Service
-    const fargateService = new ecs.FargateService(this, "FargateService", {
-        cluster,
-        taskDefinition,
-        desiredCount: 2
-    });
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
 
- }
+      this.vpc = this.importVpc ();
+      this.cluster = this.importCluster();
+      this.loadBalancerFargateService = this.createFargateMicroService01();
+      this.loadBalancerFargateService = this.createFargateMicroService02();
+    }
+
+    // method to import an exported VPC
+    private importVpc() : ec2.IVpc {
+        let vpc = ec2.Vpc.fromVpcAttributes(this, 'VPC', {
+            // Reference: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference.html
+            // The intrinsic function Fn::ImportValue returns the value of an output exported by another stack. 
+            vpcId: cdk.Fn.importValue('a001021010'),
+            availabilityZones: cdk.Fn.split(',', cdk.Fn.importValue('a001021010')),
+            publicSubnetIds: cdk.Fn.split(',', cdk.Fn.importValue('a001021010')),
+            privateSubnetIds: cdk.Fn.split(',', cdk.Fn.importValue('a001021010')),
+        });
+        return vpc;
+    }
+
+    private importCluster() : ecs.ICluster {
+        let cluster = ecs.Cluster.fromClusterAttributes(this, 'Cluster', {
+            clusterName: cdk.Fn.importValue('ClusterName'),
+            vpc: this.vpc ,
+            securityGroups: []
+        });
+        return cluster;        
+    }    
+
+    private createFargateMicroService01() : ecs_patterns.ApplicationLoadBalancedFargateService {
+        let loadBalancerFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'amazon-ecs-sample', {
+            cluster: this.cluster ,
+            cpu: 256,
+            desiredCount: 2,
+            taskImageOptions: {
+                // from  https://hub.docker.com/u/amazon
+                image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+            },
+            memoryLimitMiB: 512,
+            publicLoadBalancer: true,
+        });
+        return loadBalancerFargateService;
+    }
+    private createFargateMicroService02() : ecs_patterns.ApplicationLoadBalancedFargateService {
+        let loadBalancerFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'amazon-ecs-secrets', {
+            cluster: this.cluster ,
+            cpu: 256,
+            desiredCount: 1,
+            taskImageOptions: {
+                // from  https://hub.docker.com/u/amazon
+                image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-secrets"),
+            },
+            memoryLimitMiB: 256,
+            publicLoadBalancer: true,
+        });
+        return loadBalancerFargateService;
+    }
+    //
+
+    //
 }
